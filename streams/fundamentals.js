b816c9ba44199790e161b8309e67dfde100826a2
laripeanuts <larissarabelolf@gmail.com>
2023-02-06 18:02:48 -0300

    feat: add streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,18 +1,23 @@
-import { Readable, Writable, Transform } from "node:stream";
+// Streams ->
+
+// process.stdin
+//   .pipe(process.stdout)
+
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
-  index = 1;
+  index = 1
 
   _read() {
-    const i = this.index++;
+    const i = this.index++
 
     setTimeout(() => {
       if (i > 100) {
-        this.push(null);
+        this.push(null)
       } else {
-        const buf = Buffer.from(String(i));
+        const buf = Buffer.from(String(i))
 
-        this.push(buf);
+        this.push(buf)
       }
     }, 1000);
   }
@@ -20,19 +25,19 @@ class OneToHundredStream extends Readable {
 
 class InverseNumberStream extends Transform {
   _transform(chunk, encoding, callback) {
-    const transformed = Number(chunk.toString()) * -1;
+    const transformed = Number(chunk.toString()) * -1
 
-    callback(null, Buffer.from(String(transformed)));
+    callback(null, Buffer.from(String(transformed)))
   }
 }
 
 class MultiplyByTenStream extends Writable {
   _write(chunk, encoding, callback) {
-    console.log(Number(chunk.toString()) * 10);
-    callback();
+    console.log(Number(chunk.toString()) * 10)
+    callback()
   }
 }
 
 new OneToHundredStream()
   .pipe(new InverseNumberStream())
-  .pipe(new MultiplyByTenStream());
+  .pipe(new MultiplyByTenStream())
